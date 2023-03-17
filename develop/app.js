db4108c85f94a781127ba105bc881996a91b00b8
badermah <bmahdawi1@student.rccd.edu>
2023-03-17 15:14:12 -0700

    Added today weather box and added saved history on page

--- develop/app.js
+++ develop/app.js
@@ -5,6 +5,31 @@ const box2 = document.querySelector("#box2");
 const box3 = document.querySelector("#box3");
 const box4 = document.querySelector("#box4");
 const box5 = document.querySelector("#box5");
+const searchHistoryDiv = document.querySelector("#search-history");
+let searchHistory = new Set();
+
+if (localStorage.getItem("searchHistory")) {
+  const searchHistoryArray = JSON.parse(localStorage.getItem("searchHistory"));
+  searchHistory = new Set(searchHistoryArray);
+}
+
+function renderSearchHistory() {
+  const searchHistoryArray = Array.from(searchHistory);
+  searchHistoryDiv.innerHTML = "";
+  searchHistoryArray.forEach((city) => {
+    const button = document.createElement("button");
+    button.textContent = city;
+    button.addEventListener("click", () => {
+      form.elements.city.value = city;
+      form.dispatchEvent(new Event("submit"));
+    });
+    searchHistoryDiv.appendChild(button);
+  });
+}
+
+window.addEventListener("load", () => {
+  renderSearchHistory();
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -18,6 +43,14 @@ form.addEventListener("submit", (event) => {
       const cityName = data.city.name;
       let currentDate = "";
 
+      searchHistory.add(cityName);
+      localStorage.setItem(
+        "searchHistory",
+        JSON.stringify(Array.from(searchHistory))
+      );
+
+      renderSearchHistory();
+
       forecastData.forEach((forecast, index) => {
         if (index % 8 === 0) {
           const date = new Date(forecast.dt * 1000);
@@ -28,8 +61,10 @@ form.addEventListener("submit", (event) => {
 
           const box = eval(`box${Math.floor(index / 8) + 1}`);
           const boxHTML = `
-            <p>${weekday}</p>
-            <p><img src="${icon}" alt="${forecast.weather[0].description}">${temp}°F</p>
+          <p>${weekday}</p>
+          <p><img src="${icon}" alt="${forecast.weather[0].description}">${temp}°F</p>
+          <p>Wind: ${forecast.wind.speed} mph</p>
+          <p>Humidity: ${forecast.main.humidity}%</p>
           `;
           box.innerHTML = boxHTML;
         }
